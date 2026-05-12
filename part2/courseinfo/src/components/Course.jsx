ad143c7af7c249b2379400d9a12ff9b44f157f03
Bezawit <bezawitbiazine@gmail.com>
2026-05-12 04:41:12 -0500

    feat: update courseinfo with part 2 requirements (rendering collections)

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,35 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  
+  return (
+    <b>total of {total} exercises</b>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
