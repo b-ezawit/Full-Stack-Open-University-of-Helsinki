84d3e80462fcc8fe66280c67d2dac1ba95a056c6
bezawit <bezawitbiazine@gmail.com>
2026-07-06 03:09:22 -0500

    feat: add phonebook validation and build static production UI

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -13,8 +13,23 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type:String,
+    minLength:3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        if (!v || v.length < 8) {
+          return false
+        }
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! Format must be XX-XXXX... or XXX-XXXX... and at least 8 characters long.`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
